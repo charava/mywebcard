7d9a703e74bd7f359c96474a04529d53e6efc075
charava <charosa@nuevaschool.org>
2023-09-10 16:51:38 -0700

    building out structure

---
+++ src/App.js
@@ -0,0 +1,146 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="main-container">
+        <div className="m2">
+            <div className="top-box">
+                <h1>charlotte rosario</h1>
+            </div>
+            <div className="link-box">
+                <div className="link-box-1">
+                    <a
+                      className="App-link"
+                      href="https://nuevaschool.org/istudio"
+                      target="_blank"
+                      rel-="noopener noreferrer"
+                    >
+                      invention studio
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://smcbhrsblog.org/category/behavioral-health-commission/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      behavioral  health commission
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://www.stanfordpandastudy.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      hong lab @ CIBSR
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://communityphotobooth.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      philanthro-photography
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://epics02.wordpress.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      existential philosophy
+                    </a>
+                </div>
+                <div className="link-box-2">
+                    <a
+                      className="App-link"
+                      href="https://alsoproject.vercel.app"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      the also project
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://www.smdailyjournal.com/news/local/open-mic-for-mental-health-in-san-mateo/article_760f7d12-8283-11ed-bc31-9f21b4af9384.html"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      open mic 
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://www.youtube.com/watch?v=EpYAJdZWzZ4"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      documentary
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://nuevaschool.org/istudio"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      greenswing
+                    </a>
+                    <a
+                      className="App-link"
+                      href="https://mrobotics8381.wordpress.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      robots
+                    </a>
+                </div>
+            </div>
+            <div className="bottom-box">
+                <p className="email-p">charlottearosario@gmail.com</p>
+                <div className="social-links">
+                    <a
+                        className="App-link"
+                        href="https://mrobotics8381.wordpress.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        linkedin
+                    </a>
+                    <a
+                        className="App-link"
+                        href="https://mrobotics8381.wordpress.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        github
+                    </a>
+                    <a
+                        className="App-link"
+                        href="https://mrobotics8381.wordpress.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        youtube
+                    </a>
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/* 
+
+email - charlottearosario@
+
+linkedin
+instagram
+youtube
+github
+calendly
+send me an email button 
+
+
+*/
