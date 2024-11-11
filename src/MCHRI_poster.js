3bb4c7069a791a9790c15b4425f14a320fffa7e5
Charlotte <charlottearosario@gmail.com>
2024-11-11 12:36:07 -0800

    stanford mchri

---
+++ src/MCHRI_poster.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MCHRI = () => {
+  return (
+    <div>
+       <div className="main-container">
+        <img
+                    src={"other-assets/Stanford MCHRI - Charlotte Rosario.jpeg"}
+                    alt={`Stanford MCRHI Poster - Charlotte Rosario`}
+                    style={{ width: "auto", height: "95vh" }} // Adjust the width as needed
+
+                />
+      </div>
+    </div>
+  );
+}
+
+export default MCHRI;
